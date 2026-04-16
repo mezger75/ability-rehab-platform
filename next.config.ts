1948df7f3e96c1efd44c863a1f42c1d87afde298
mezger75 <9771804@gmail.com>
2026-04-16 20:25:35 +0300

    chore: added yandex api key to next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     OPENROUTER_API_KEY_1: process.env.OPENROUTER_API_KEY_1,
+    YANDEX_API_KEY: process.env.YANDEX_API_KEY,
   },
 };
 
