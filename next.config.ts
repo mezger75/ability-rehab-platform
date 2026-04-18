c2f3f80af742d9834769f93d6865141f2507b1c1
mezger75 <9771804@gmail.com>
2026-04-18 12:18:22 +0300

    fix: env vars to nextconfig

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   env: {
     OPENROUTER_API_KEY_1: process.env.OPENROUTER_API_KEY_1,
     YANDEX_API_KEY: process.env.YANDEX_API_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
 };
 
